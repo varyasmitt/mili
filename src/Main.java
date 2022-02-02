11133e229b0925ede58a79808e3539541e5883b5
varyasmitt <varyasmitt@icloud.com>
2022-02-02 20:19:26 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 40;
+        int miles = cost / 20;
+
+        System.out.println("Начислено миль " + miles);
+
+    }
+}
